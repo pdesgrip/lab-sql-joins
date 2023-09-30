ebf5230b955ecda15d2b473d5261965de51c2771
Paul Desgrippes <pdesgrip@gmail.com>
2023-09-30 18:02:43 +0200

    Solution

--- SQL_JOINS.sql
+++ SQL_JOINS.sql
@@ -22,3 +22,49 @@ FROM store AS s
 JOIN staff AS st ON s.store_id = st.store_id
 JOIN payment as p ON st.staff_id = p.staff_id
 GROUP BY s.store_id;
+
+-- Determine the average running time by category
+
+SELECT c.name AS category, AVG(f.length) AS avg_running_time
+FROM category AS c
+JOIN film_category AS fc ON c.category_id = fc.category_id
+JOIN film AS f ON fc.film_id = f.film_id
+GROUP BY c.name;
+
+-- Indentify the the film categories with the longest average running time
+
+SELECT c.name AS category, AVG(f.length) AS avg_running_time
+FROM category AS c
+JOIN film_category AS fc ON c.category_id = fc.category_id
+JOIN film AS f ON fc.film_id = f.film_id
+GROUP BY c.name
+ORDER BY avg_running_time DESC;
+
+-- Display the top 10 most frequently rented movies (desc order)
+
+SELECT f.title, COUNT(r.inventory_id) AS total_rentals
+FROM film AS f
+JOIN inventory AS i ON f.film_id = i.film_id
+JOIN rental AS r ON i.inventory_id = r.inventory_id
+GROUP BY f.title
+ORDER BY total_rentals DESC
+LIMIT 10;
+
+
+-- Acadmey Dinosaur available
+
+SELECT f.title, COUNT(i.inventory_id) AS num_of_copies
+FROM film AS f
+JOIN inventory AS i ON f.film_id = i.film_id
+WHERE f.title = 'Academy Dinosaur' AND i.store_id = 1
+GROUP BY f.title;
+
+
+-- List of all distinct film titles, along with their availability status in the inventory.
+
+
+SELECT film.title AS film_title,
+    CASE WHEN IFNULL(inventory.inventory_id, 0) > 0 THEN 'Available'
+	ELSE 'NOT available'
+    END AS status
+FROM film JOIN inventory ON film.film_id = inventory.film_id;
